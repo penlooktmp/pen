1e38f30ebc9803dbcc88e17a164a17c368a65422
loint <loint@penlook.com>
2015-03-30 07:40:19 +0700

    Panic an error when template syntax error

--- system/view.go
+++ system/view.go
@@ -11,20 +11,15 @@ type View struct {
 }
 
 func (view View) ToContext() engine.Context {
-    return engine.Context{"key":"value"}
+    context := engine.Context{}
+    //for key, value := range view.
 }
 
 func (view View) Render(html HTML) {
-
     var template = engine.Must(engine.FromFile("view/" + view.Http.Controller + "/" + view.Http.Action +".html"))
-    template.ExecuteWriter(view.ToContext(), view.Http.Response)
+    err := template.ExecuteWriter(view.ToContext(), view.Http.Response)
 
-	/*
-	t := template.New("nonexported template demo")
-    t, _ = t.Parse("hello {{.Name}}! Age is {{.nonExportedAgeField}}.")
-    err := t.Execute(os.Stdout, p)
     if err != nil {
-        fmt.Println("There was an error:", err.String())
-    }*/
-
+        panic(err)
+    }
 }
