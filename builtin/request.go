b4266c7a644125439a55da45a329a8ff5ae5fcc9
loint <loint@penlook.com>
2015-04-01 00:05:48 +0700

    Decoupled controller components

---
+++ builtin/request.go
@@ -0,0 +1,16 @@
+package pongo
+
+func (controller Controller) GET(key string) string {
+}
+
+func (controller Controller) POST(key string) string {
+}
+
+func (controller Controller) PUT(key string) string {
+}
+
+func (controller Controller) PATCH(key string) string {
+}
+
+func (controller Controller) DELETE(key string) string {
+}
