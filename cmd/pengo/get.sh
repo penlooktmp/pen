57a25d68b15a2869e85624c015fe8aea686b4b6d
loint <loint@penlook.com>
2015-05-18 00:02:40 +0700

    Remove pengo compiler

--- cmd/pengo/get.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-go get "github.com/stretchr/testify/assert"
-go get "github.com/codegangsta/cli"
-go get "github.com/flosch/pongo2"
-go get "github.com/julienschmidt/httprouter"
-go get "github.com/olekukonko/tablewriter"
-go get "github.com/oleiade/lane"
