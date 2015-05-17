4e898d800c7dbb49d9ab3358dc6f33d00c8c1def
loint <loint@penlook.com>
2015-05-18 02:30:46 +0700

    Using Makefile to build compiler in cpp for pengo cli

--- cmd/src/main.cpp
+++ cmd/src/main.cpp
@@ -23,7 +23,7 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include "cmdline.hpp"
+#include <lib/cmdline.hpp>
 
 #include <iostream>
 using namespace std;
