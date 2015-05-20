c43b8a095911535fbcd02a8e3e0d6ca0b57f4d3a
loint <loint@penlook.com>
2015-05-20 17:07:21 +0700

    Decoupled wpp library for g++ faster compilation

---
+++ cmd/src/cli/cli.cpp
@@ -0,0 +1,46 @@
+/**
+ * Pengo Project
+ *
+ * Copyright (c) 2015 Penlook Development Team
+ *
+ * --------------------------------------------------------------------
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * --------------------------------------------------------------------
+ *
+ * Author:
+ *     Loi Nguyen       <loint@penlook.com>
+ */
+
+#include <cli/cli.h>
+
+Cli::Cli(int argc, char* argv[])
+{
+	cout << "Init parameter";
+}
+
+Cli &Cli::add(string name, string desc, cli_callback func)
+{
+	callback[name] = func;
+	//option[name] = option_value;
+	return *this;
+}
+
+Cli &Cli::run()
+{
+	cout << "Cli run !";
+	return *this;
+}
---
+++ cmd/src/http/http.cpp
@@ -0,0 +1,50 @@
+/**
+ * Pengo Project
+ *
+ * Copyright (c) 2015 Penlook Development Team
+ *
+ * --------------------------------------------------------------------
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * --------------------------------------------------------------------
+ *
+ * Author:
+ *     Loi Nguyen       <loint@penlook.com>
+ */
+
+#include <http/http.h> 
+
+template <typename Callback>
+void Http::get(string router, Callback http_callback)
+{
+    server.get(router, &http_callback);
+}
+
+template <typename Callback>
+void Http::post(string router, Callback http_callback)
+{
+    server.get(router, &http_callback);
+}
+
+void Http::listen(int port = 80)
+{
+    try {
+        cout << "Listening on port " << port << endl;
+        server.start(port);
+    } catch(Exception e) {
+        cerr << "WebServer: " << e.what() << endl;
+    }
+}
